0a4a17661b4ca07956c8a5f379ce381053e18af1
Daniel Bush <dlb.id.au@gmail.com>
2010-06-22 12:36:28 +1000

    acts_as_image_file now takes parameters including image name; added instance methods to the AR class using it.  All untested.

--- lib/acts_as_image_file.rb
+++ lib/acts_as_image_file.rb
@@ -1,15 +1,103 @@
 
 module ActsAsImageFile
+
   def self.included base
     base.send :extend , ClassMethods
   end
+
   module ClassMethods
-    def acts_as_image_file
-      send :include, InstanceMethods
+
+    # Access the image db.
+    # 
+    # Example:
+    #   ar = AR.new
+    #   AR.db.absolute(ar.name,:width => 60)
+    #   AR.db.fetch(ar.name,:width => 60)
+
+    def db
+      @@db
     end
+
+    # Invoked by the AR class that wants this behaviour.
+    #
+    # +params+ is a hash
+    # :hooks      => hook object which uses ImageDb::DB.hooks
+    #                functionality
+    # :rel_root   => alternative root such as an http alias
+    #                eg '/http/alias'
+    #                as used by the ImageDb::DB system.
+    # :name_field => :name (default)
+    #                Name of the field holding the image name.
+    #                eg of image name: 'image-1.jpg'
+
+    def acts_as_image_file root , params=nil
+
+      @@acts_as_image_file = params.nil? ? {:name_field => :name} : params
+      @@db = ImageDb::DB.new(root,@@acts_as_image_file[:rel_root])
+      @@db.hooks = @@acts_as_image_file[:hooks]
+
+      # Name field should be unique and not null...
+
+      send :validates_uniqueness_of , @@acts_as_image_file[:name_field]
+      send :validates_presence_of , @@acts_as_image_file[:name_field]
+
+      # Create some instance methods for this AR class:
+
+      self.module_eval do
+
+        # Convenience method to access image db from an instance.
+
+        def db
+          self.class.db
+        end
+
+        # Store an original image...
+
+        def store filepath,params=nil
+          db.store filepath,params
+        end
+
+        # Retrieve url for original image...
+
+        def url params=nil
+          db.resolve self.send(@@acts_as_image_file[:name_field]) , params
+        end
+
+        # Rename images if name is changed.
+        # 
+        # If image is new or name not set then don't do anything.
+
+        def after_save
+          STDERR.puts 'after_destroy'
+          return true unless db
+          r = self.changes[@@acts_as_image_file[:name_field]]
+          return true unless r
+          old,new = r
+          unless old.nil?
+            db.rename(old,new,:force => true) if db
+          end
+        end
+
+        # Do nothing.  Up to user if they want to destroy images.
+
+        def after_destroy
+          STDERR.puts 'after_destroy'
+          #db.delete self.name
+        end
+
+        # Check original image file exists.
+
+        def file_exists? params=nil
+          r = db.fetch(self.send(@@acts_as_image_file[:name_field]) , params)
+          r.nil? ? false : true
+        end
+
+      end # module_eval
+
+    end # acts_as_image_file
+
   end
-  module InstanceMethods
-  end
+
 end
 
 ActiveRecord::Base.send :include , ActsAsImageFile
--- test/acts_as_image_file_test.rb
+++ test/acts_as_image_file_test.rb
@@ -1,13 +1,26 @@
 require 'test_helper'
+require 'fileutils'
+
+FileUtils.rm_rf '/tmp/acts_as_image_file_tests'
+FileUtils.mkdir_p '/tmp/acts_as_image_file_tests'
 
 class ActsAsImageFileTest < ActiveSupport::TestCase
   load_schema
+
   class Image < ActiveRecord::Base
-    acts_as_image_file
+    acts_as_image_file '/tmp/acts_as_image_file_tests/r1'
   end
+
   class Tag < ActiveRecord::Base
   end
+
   class TaggedImage < ActiveRecord::Base
+  end
+
+  def setup
+    Image.delete_all
+    Tag.delete_all
+    TaggedImage.delete_all
   end
 
   test "schema loads correctly" do
@@ -15,5 +28,15 @@ class ActsAsImageFileTest < ActiveSupport::TestCase
     assert_equal [],Tag.all
     assert_equal [],TaggedImage.all
   end
+
+  test "changing name" do
+    i = Image.new
+    i.name = 'foo.jpg'
+    i.save!
+    i.name = 'bar.jpg'
+    i.save!
+    p i.url
+  end
+
 
 end
