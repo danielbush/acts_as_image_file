8c9a45853c0f28ef67a5a968c4a72811ec012268
Daniel Bush <dlb.id.au@gmail.com>
2010-09-24 09:15:49 +1000

    Load rails first then require anything extra - otherwise we may get errors regarding the expected version of rails (especially if you have multiple and later versions of rails installed via gems).

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,3 @@
-require 'rubygems'
-require 'active_support'
-require 'active_support/test_case'
 
 ENV['RAILS_ENV'] = 'test'
 ENV['RAILS_ROOT'] ||= File.dirname(__FILE__) + '/../../../..'
@@ -8,7 +5,12 @@ ENV['RAILS_ROOT'] ||= File.dirname(__FILE__) + '/../../../..'
 require 'test/unit'
 require File.expand_path(File.join(ENV['RAILS_ROOT'], 'config/environment.rb'))
 
+#require 'rubygems'
+require 'active_support'
+require 'active_support/test_case'
+
 def load_schema
+
   config = YAML::load(IO.read(File.dirname(__FILE__) + '/database.yml'))
   ActiveRecord::Base.logger = Logger.new(File.dirname(__FILE__) + '/debug.log')
 
